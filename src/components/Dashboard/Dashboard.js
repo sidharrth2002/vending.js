a8e46698a3534ae23e10c117c34acc837900da06
Mak Yen Wei <shaonmak@gmail.com>
2021-03-05 18:06:46 +0800

    status: json data parsing pending

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Table } from 'antd';
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -17,10 +17,11 @@ const { SubMenu } = Menu;
 
 const Dashboard = (props) => {
     const user = useSelector(state => state.auth.user)
+    const accessToken = useSelector(state => state.auth.token);
     const [collapsed, setCollapsed] = useState(false)
     const dispatch = useDispatch();
     const refreshToken = useSelector(state => state.auth.refreshToken);
-    console.log(refreshToken);
+    console.log(`access token ` + accessToken);
     const logout = () => {
         // axios.post('http://localhost:5000/v1/auth/logout', {
         //     refreshToken: refreshToken
@@ -33,41 +34,97 @@ const Dashboard = (props) => {
         dispatch(LOGOUT());
     }
 
-    return (
-        <Layout style={{ minHeight: '100vh' }}>
-          <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
-            <div className="logo"> 
-                Vending.js
-            </div>
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              <Menu.Item key="1" icon={<PieChartOutlined />}>
-                Complaints
-              </Menu.Item>
-              <Menu.Item key="2" icon={<DesktopOutlined />}>
-                Technicians
-              </Menu.Item>
-              <Menu.Item key="3" icon={<FileOutlined />}>
-                Appointments
-              </Menu.Item>
-              <Menu.Item key="4" icon={<LogoutOutlined />} onClick={() => logout()}>
-                  Logout
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }} />
-            <Content style={{ margin: '0 16px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb>
-              <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                Bill is a cat.
-              </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-          </Layout>
-        </Layout>    );
+    const columns = [
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        width: 150,
+      },
+      {
+        title: 'Date Created',
+        dataIndex: 'date',
+        width: 150,
+      },
+      {
+        title: 'Remarks',
+        dataIndex: 'remarks',
+      },
+      {
+        title: 'Urgency',
+        dataIndex: 'urgency',
+        width: 150,
+      }
+    ];
+
+  const data = [];
+  axios.get('http://localhost:5000/v1/complaint', {
+      headers: { Authorization: `Bearer ${accessToken}` 
+    }
+  })
+  .then(function (res){
+
+    console.log(res)
+
+    let i = 0;
+
+    res.data.forEach(el => {
+      data.push({
+        key: i,
+        title: el.body,
+        date: el.createdAt,
+        remarks: el.remarks,
+      });
+      i++
+    });
+
+    console.log(data);
+
+  })
+  .catch(function (err){
+
+    console.log(err)
+
+  })
+
+  return (
+      
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
+        <div className="logo"> 
+            Vending.js
+        </div>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Complaints
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Technicians
+          </Menu.Item>
+          <Menu.Item key="3" icon={<FileOutlined />}>
+            Appointments
+          </Menu.Item>
+          <Menu.Item key="4" icon={<LogoutOutlined />} onClick={() => logout()}>
+              Logout
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 240 }} />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>    
+
+  );
+
 };
 
 export default Dashboard;
