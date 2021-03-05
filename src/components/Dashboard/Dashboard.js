fb8179a8f04b292e5628dad6628cb212177112c5
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-05 16:41:33 +0800

    feat: simple layout

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const Dashboard = () => {
+    const user = useSelector(state => state.auth.user)
+    const [collapsed, setCollapsed] = useState(false)
+
     return (
-        <div>
-            Dashboard here
-        </div>
-    );
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
+            <div className="logo"> 
+                Vending.js
+            </div>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<PieChartOutlined />}>
+                Complaints
+              </Menu.Item>
+              <Menu.Item key="2" icon={<DesktopOutlined />}>
+                Technicians
+              </Menu.Item>
+              <Menu.Item key="9" icon={<FileOutlined />}>
+                Appointments
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout">
+            <Header className="site-layout-background" style={{ padding: 0 }} />
+            <Content style={{ margin: '0 16px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              </Breadcrumb>
+              <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                Bill is a cat.
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
+        </Layout>    );
 };
 
 export default Dashboard;
