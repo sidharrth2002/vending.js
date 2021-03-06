77aedd38b7a8608287803a532e5c5863ed16591a
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-07 03:09:49 +0800

    what point

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
@@ -22,8 +22,10 @@ const Routing = props => {
     }, [])
 
     return (
-        <div>
-            <img src={{}}
+        <div style={{textAlign: 'center'}}>
+            <h1>Take this route if you want to be smart!</h1>
+            <h3>Future versions will be interactive</h3>
+            <img style={{maxWidth: '400px', textAlign: 'center'}} src={imageURL} /> 
         </div>
     );
 };
