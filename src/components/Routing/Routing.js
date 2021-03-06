e86f735cce010a3b8ecfbd0db911f86d24045d4a
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-07 02:44:32 +0800

    heroku

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const Routing = (props) => {
+const Routing = props => {
+    const user = useSelector(state => state.auth.user)
+
+    useEffect(() => {
+        console.log(user)
+        axios.post(`${process.env.REACT_APP_API_URL}/v1/map/getroute/${user.id}`, {
+            latitude: 2.9213,
+            longitude: 101.6559
+        })
+        .then(response => {
+            if(response.status == 200) {
+                console.log(response.data);
+            }
+        })
+    }, [])
+
     return (
         <div>
-            
+            sfsdssf
         </div>
     );
+};
+
+Routing.propTypes = {
+    
 };
 
 export default Routing;
